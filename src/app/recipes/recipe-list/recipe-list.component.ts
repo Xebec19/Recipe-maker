70640b3421d47ba636ef415f63896de1fd5b1c7c
xebec19 <thakur190420@gmail.com>
2021-04-09 23:05:10 +0530

    setting up layout

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is simply a test','https://images.unsplash.com/photo-1504388192519-fb4be897c4d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
