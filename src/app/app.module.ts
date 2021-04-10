9925afe37c7566e31f21f0200428dde9925cebd1
xebec19 <thakur190420@gmail.com>
2021-04-10 23:04:54 +0530

    in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerElementComponent } from './server-element/server-element.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerElementComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() blueprintCreated = new EventEmitter<{serverName:string,serverContent:string}>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
