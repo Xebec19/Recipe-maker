3679a185a2f0ace5cdece33e7f60a62bfe86f5d9
xebec19 <thakur190420@gmail.com>
2021-04-18 10:02:54 +0530

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    RecipeListComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
