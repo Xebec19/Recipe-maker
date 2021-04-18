65d00f90e039c7874c14ab0fbecbb6e8f48e52ec
xebec19 <thakur190420@gmail.com>
2021-04-18 09:12:29 +0530

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerElementComponent } from './server-element/server-element.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerElementComponent,
-    CockpitComponent
   ],
   imports: [
     BrowserModule,
